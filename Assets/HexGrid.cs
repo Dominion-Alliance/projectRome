1b66dffb1a88ada308bd816b8586830cb7ead3f5
ChancellorBill.Caprica <chancellorbill@outlook.com>
2016-10-20 23:29:28 -0500

    Starting working on creating a mesh. Hex map will be based on it. Just experimenting for now. TestRun_Data folder has been removed from repository and should no longer be tracked after this commit.

--- Assets/HexGrid.cs
+++ Assets/HexGrid.cs
@@ -3,5 +3,27 @@ using System.Collections;
 
 public class HexGrid : MonoBehaviour
 {
-    
+    public Transform tilePrefab;
+    public Vector2 mapSize;
+
+    [Range(0,1)]
+    public float outlinePrecent;
+
+    void Start()
+    {
+        generateGrid();
+    }
+
+    public void generateGrid()
+    {
+        for (int x = 0; x <mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y + 0.5f + y);
+                Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
+                newTile.localScale = Vector3.one * (1 - outlinePrecent);
+            }
+        }
+    }
 }
