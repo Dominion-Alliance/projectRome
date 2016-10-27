7ffbb721077ca41453eed01dc392c5e7af19da56
jt2529 <jj_t_2529@yahoo.com>
2016-10-26 20:04:09 -0500

    Added a 'Base' prefab, and a new folder 'Game Pieces'
    Moved health bar and other related assets to the new folder, and added a
    base prefab which utilizes the playerHealthBar (ought to be renamed
    soon).

---
+++ Assets/Game Pieces/Base.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Base : MonoBehaviour {
+    public HealthBar healthBar;
+    public int level = 1;
+    public int radius = 0;
+    public bool active = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void levelUp() {
+        level++;
+        int r = level / 3;
+        radius = r;
+    }
+
+    public void changeHealth(int a) {
+        healthBar.adjustHealth(a);
+        if (healthBar.currentHealth <= 0) {
+            destroyBase();
+        }
+
+    }
+
+    public void destroyBase() {
+        active = false;
+    }
+}
---
+++ Assets/Game Pieces/HealthBar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour
+{
+    public int startingHealth = 100;                            // The amount of health the player starts the game with.
+    public int currentHealth = 100;                                   // The current health the player has.
+	public Transform  tran;
+
+    void Start()
+    {
+        gameObject.tag = "localPlayerHealth";
+    }
+
+    void Update()
+    {
+
+    }
+		
+    public void adjustHealth(int amount)
+    {
+        // Reduce the current health by the damage amount.
+        currentHealth += amount;
+
+    }
+
+	public void SetLocation(Vector3 position)
+	{
+		tran.position = position;
+	}
+		
+}
